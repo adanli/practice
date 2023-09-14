55f4e85ddeaa5cff76f4f8cf5ea56a16170cb3b6
li adan <adanli@126.com>
2023-09-14 17:59:09 +0800

    init

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,5 +3,22 @@ package cn.egg
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+
+    val hello = "hello world!"
+    println(hello)
+
+    Array(1, 2, 3).filter(_ != 2).foreach(println(_))
+
+    val str: String =
+      """today is
+        |a good
+        |day""".stripMargin.replaceAll("\n", " ")
+    println(str)
+
+    str.split(' ').foreach(println)
+
+    val name = "aaa"
+    println(s"his name is ${name.length} characters long")
+
   }
 }
